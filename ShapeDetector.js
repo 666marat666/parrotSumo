115643ba95913a32c70a150645c5f4228f740add
Steven BOEHM <boehm_s@etna-alternance.net>
2016-03-09 11:42:36 +0100

    square detection

--- ShapeDetector.js
+++ ShapeDetector.js
@@ -25,82 +25,98 @@ video.on("data", function(data) {
 
 
 function launchShapeDetection () {
-	setInterval(function() {
-	  if (buf == null) {
-	   return;
-	  }
-
-	  try {
-	  	console.log("--- New Iteration ---\n");
-	  	shapeDetection();
-
-	  } catch(e) {
+    setInterval(function() {
+	if (buf == null) {
+	    return;
+	}
+	
+	try {
+	    console.log("--- New Iteration ---\n");
+	    shapeDetection();
+	    
+	} catch(e) {
 	    console.log(e);
-	  }
-	}, 500);
+	}
+    }, 500);
 }
 
 
 function shapeDetection() {
-var lowThresh = 0;
-var highThresh = 100;
-var nIters = 2;
-var minArea = 2000;
+    var lowThresh  = 0;
+    var highThresh = 100;
+    var nIters     = 2;
+    var minArea    = 2000;
+    
+    var BLUE  = [0, 255, 0]; //B, G, R
+    var RED   = [0, 0, 255]; //B, G, R
+    var GREEN = [0, 255, 0]; //B, G, R
+    var WHITE = [255, 255, 255]; //B, G, R
+    
+    var display = false;
 
-var BLUE = [0, 255, 0]; //B, G, R
-var RED   = [0, 0, 255]; //B, G, R
-var GREEN = [0, 255, 0]; //B, G, R
-var WHITE = [255, 255, 255]; //B, G, R
-
-
-cv.readImage(buf, function(err, im) {
-
+    cv.readImage(buf, function(err, im) {
 	var out = new cv.Matrix(im.height(), im.width());
-
+	
 	//im.convertGrayscale();
 	var im_canny = im.copy();
-
+	
 	im_canny.canny(lowThresh, highThresh);
 	im_canny.dilate(nIters);
-
+	
 	var contours = im_canny.findContours();
 	console.log("Contours count : " + contours.size())
 	var validAreas = 0;
 	for(var i = 0; i < contours.size(); i++) {
-
-		if(contours.area(i) < minArea) continue;
-		validAreas ++;
-		var arcLength = contours.arcLength(i, true);
-		contours.approxPolyDP(i, 0.017 * arcLength, true);
+	    
+	    if(contours.area(i) < minArea) continue;
+	    validAreas ++;
+	    var arcLength = contours.arcLength(i, true);
+	    contours.approxPolyDP(i, 0.017 * arcLength, true);
+	    
+	    switch(contours.cornerCount(i)) {
+	    case 3:
+		im.drawContour(contours, i, GREEN);
 		
-		switch(contours.cornerCount(i)) {
-		case 3:
-			im.drawContour(contours, i, GREEN);
-
-			break;
-		case 4:
-			im.drawContour(contours, i, RED);
-			var points = [
-      			contours.point(i, 0),
-      			contours.point(i, 1),
-      			contours.point(i, 2),
-      			contours.point(i, 3)
+		break;
+	    case 4:
+		im.drawContour(contours, i, RED);
+		var points = [
+      		    contours.point(i, 0),
+      		    contours.point(i, 1),
+      		    contours.point(i, 2),
+      		    contours.point(i, 3)
     		]
-			console.log("Point 0 : [" + points[0].x + ", " + points[0].y + "]");
-			break;
-		default:
-			break;
-			//im.drawContour(contours, i, WHITE);
-		}
+		points.sort(compare);
+		console.log(points);
+		if (Math.abs(Math.abs(points[0].x - points[3].x) - Math.abs(points[0].y - points[3].y)) < 50)
+		    display = true;
+		break;
+	    default:
+		break;
+		//im.drawContour(contours, i, WHITE);
+	    }
 	}
-	w.show(im);
+	
+	if (display == true) {
+	    var im2 = im.crop(points[0].x, points[0].y, (points[3].x - points[0].x), (points[3].y - points[0].y));
+	    w.show(im2);
+	} else
+	    w.show(im);
 	w.blockingWaitKey(0,50);
 	console.log("Valid areas : " + validAreas)
 	
-//	out.save('./out.png');
-});
+	//	out.save('./out.png');
+    });
 }
 
 
 
 
+function compare(a,b) {
+  if ((a.x + a.y) < (b.x + b.y))
+    return -1;
+  else if ((a.x + a.y) > (b.x + b.y))
+    return 1;
+  else 
+    return 0;
+}
